3764917b8e5cf4c7ea1032c601a56cc676dbcc68
anantshende01 <anantshende01@gmail.com>
2023-05-02 11:42:23 +0530

    first commit

---
+++ CustomerCare/src/test/java/com/ibm/customerCare/CustomerCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.customerCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
